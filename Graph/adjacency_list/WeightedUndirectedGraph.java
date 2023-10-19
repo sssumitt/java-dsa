8ec416c6c79509eb5edd18738d175542d9dcb41f
ssumitt <sumit.2022@vitstudent.ac.in>
2023-10-19 15:50:06 +0530

    changed formate

--- Graph/adjacency_list/WeightedUndirectedGraph.java
+++ Graph/adjacency_list/WeightedUndirectedGraph.java
@@ -1,79 +1,70 @@
 
 package Graph.adjacency_list;
+
 import java.util.*;
-  
-  public class  WeightedUndirectedGraph {
 
-    // undirected 
+public class WeightedUndirectedGraph {
 
-//__________________________________________________
-    
-    public static class Edge{
-      int src ,dest,weight;
-    // constructor of edge 
-      Edge(int source, int destination,int weight) {
+  // undirected
 
-        this.src= source ;
-        this.dest = destination ; 
-        this.weight= weight ;
+  // __________________________________________________
 
-      }
+  public static class Edge {
+    int src, dest, weight;
 
-    }
+    // constructor of edge
+    Edge(int source, int destination, int weight) {
 
-//___________________________________________________
-
-
-//To create  graph 
-
-   
-    public static void CreatGraph(ArrayList<Edge> graph[]) {
-      // as array ((graph[])contains all null values 
-
-      // array(graph[]) need to initialize with empty edges
-
-      for ( int i =0 ; i< graph.length ; i++) {
-        
-        graph[i] =  new ArrayList<Edge>();
-        
-      }
-
-      graph[0].add(new Edge(0,2,1));
-
-      graph[1].add(new Edge(1,2,-1));
-      graph[1].add(new Edge(1,3,2));
-
-      graph[2].add(new Edge(2,0,3));
-      graph[2].add(new Edge(2,1,3));
-      graph[2].add(new Edge(2,3,4));
-
-      graph[3].add(new Edge(3,1,2));  
-      graph[3].add(new Edge(3,1,2));      
-  
-    }
-    
-
-
-
-
-
-
-
-    public static void main (String args[]) {
-      // no of vertix
-      int V = 4; 
-      ArrayList<Edge> graph[] = new ArrayList[V];
-      
-
-      CreatGraph(graph);
-
-
-       // print 2's neighbour
-      for(int i =0 ;i < graph[2].size(); i++) {
-        Edge e = graph[2].get(i);
-        System.out.println("dest-> "+e.dest+" src-> " +e.src +" weight-> "+e.weight);
-
-      }
+      this.src = source;
+      this.dest = destination;
+      this.weight = weight;
 
     }
+
   }
+
+  // ___________________________________________________
+
+  // To create graph
+
+  public static void CreatGraph(ArrayList<Edge> graph[]) {
+    // as array ((graph[])contains all null values
+
+    // array(graph[]) need to initialize with empty edges
+
+    for (int i = 0; i < graph.length; i++) {
+
+      graph[i] = new ArrayList<Edge>();
+
+    }
+
+    graph[0].add(new Edge(0, 2, 1));
+
+    graph[1].add(new Edge(1, 2, -1));
+    graph[1].add(new Edge(1, 3, 2));
+
+    graph[2].add(new Edge(2, 0, 3));
+    graph[2].add(new Edge(2, 1, 3));
+    graph[2].add(new Edge(2, 3, 4));
+
+    graph[3].add(new Edge(3, 1, 2));
+    graph[3].add(new Edge(3, 1, 2));
+
+  }
+
+  public static void main(String args[]) {
+    // no of vertix
+    int V = 4;
+    ArrayList<Edge> graph[] = new ArrayList[V];
+
+    CreatGraph(graph);
+
+    // print 2's neighbour
+    for (int i = 0; i < graph[2].size(); i++) {
+      Edge e = graph[2].get(i);
+      System.out.println("dest-> " + e.dest + " src-> " + e.src + " weight-> " + e.weight);
+
+    }
+
+  }
+}
--- Graph/adjacency_list/nonWeightedUndirectedGraph.java
+++ Graph/adjacency_list/nonWeightedUndirectedGraph.java
@@ -1,135 +1,122 @@
 package Graph.adjacency_list;
+
 import java.util.*;
-  
-  public class  nonWeightedUndirectedGraph {
 
-    // undirected 
+public class nonWeightedUndirectedGraph {
 
-//__________________________________________________
-    
-    public static class Edge{
-      int src ,dest;
-    // constructor of edge 
-      Edge(int source, int destination) {
+  // undirected
 
-        this.src= source ;
-        this.dest = destination ; 
+  // __________________________________________________
 
-      }
+  public static class Edge {
+    int src, dest;
+
+    // constructor of edge
+    Edge(int source, int destination) {
+
+      this.src = source;
+      this.dest = destination;
 
     }
 
-//___________________________________________________
+  }
 
+  // ___________________________________________________
 
-//To create  graph 
-public static void addEdge(ArrayList<Edge> graph[],int src,int dest) {
-  graph[src].add(new Edge(src,dest));
+  // To create graph
+  public static void addEdge(ArrayList<Edge> graph[], int src, int dest) {
+    graph[src].add(new Edge(src, dest));
 
-}
+  }
 
-   
-    public static void CreatGraph(ArrayList<Edge> graph[]) {
-      // as array ((graph[])contains all null values 
+  public static void CreatGraph(ArrayList<Edge> graph[]) {
+    // as array ((graph[])contains all null values
 
-      // array(graph[]) need to initialize with empty edges
+    // array(graph[]) need to initialize with empty edges
 
-      for ( int i =0 ; i< graph.length ; i++) {
-        
-        graph[i] =  new ArrayList<Edge>();
-        
-      }
-      addEdge(graph, 0, 1);
-      addEdge(graph, 0, 2);
+    for (int i = 0; i < graph.length; i++) {
 
+      graph[i] = new ArrayList<Edge>();
 
-      addEdge(graph, 1, 0);
-      addEdge(graph, 1, 3);
-
-      addEdge(graph, 2, 0);
-      addEdge(graph,  2, 4);
-
-      addEdge(graph, 3, 1);
-      addEdge(graph,  3, 4);
-      addEdge(graph, 3, 5);
-
-
-      addEdge(graph,  4, 2); 
-      addEdge(graph, 4, 3);
-      addEdge(graph,  4, 5);
-
-      addEdge(graph, 5, 3);
-      addEdge(graph,  5, 4);
-      addEdge(graph, 5, 6);
-      
-
-            
     }
-
-
     
-    public static void bfs(ArrayList<Edge> graph[]) {
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
 
-      Queue<Integer> q = new LinkedList<>();
-      boolean visited[] = new boolean[graph.length];
-      q.add(0);
+    addEdge(graph, 1, 0);
+    addEdge(graph, 1, 3);
 
-      while(q.size()!=0) {
+    addEdge(graph, 2, 0);
+    addEdge(graph, 2, 4);
 
-        int curr =q.remove();
+    addEdge(graph, 3, 1);
+    addEdge(graph, 3, 4);
+    addEdge(graph, 3, 5);
 
-        if(visited[curr]==false) {
+    addEdge(graph, 4, 2);
+    addEdge(graph, 4, 3);
+    addEdge(graph, 4, 5);
 
-          //print the current element in graph
-          System.out.print(curr+" ");
+    addEdge(graph, 5, 3);
+    addEdge(graph, 5, 4);
+    addEdge(graph, 5, 6);
 
-          //to check visted node should not be printed again
-          visited[curr]= true;
+  }
 
-          //to find all the destination of curr vertex then add in queue
-          for(int i =0 ; i < graph[curr].size(); i++) {
-            
-            Edge e = graph[curr].get(i);
-            q.add(e.dest);
+  public static void bfs(ArrayList<Edge> graph[]) {
 
+    Queue<Integer> q = new LinkedList<>();
+
+    boolean visited[] = new boolean[graph.length];
+    q.add(0);
+
+    while (q.size() != 0) {
+
+      int curr = q.remove();
+
+      if (visited[curr] == false) {
+
+        // print the current element in graph
+        System.out.print(curr + " ");
+
+        // to check visted node should not be printed again
+        visited[curr] = true;
+
+        // to find all the destination of curr vertex then add in queue
+        for (int i = 0; i < graph[curr].size(); i++) {
+
+          Edge e = graph[curr].get(i);
+          q.add(e.dest);
 
         }
-      
 
       }
 
-      }
-
-
-
-    }
-    public static void dfs(ArrayList<Edge>graph[],int curr, boolean vis[]) {
-      System.out.print(curr+" ");
-      vis[curr]= true;
-
-      for(int i=0; i <graph[curr].size(); i++) {
-
-        Edge e= graph[curr].get(i);
-        if(vis[e.dest]==false)dfs(graph, e.dest, vis);
-
     }
 
+  }
 
+  public static void dfs(ArrayList<Edge> graph[], int curr, boolean vis[]) {
+    System.out.print(curr + " ");
+    vis[curr] = true;
 
+    for (int i = 0; i < graph[curr].size(); i++) {
+
+      Edge e = graph[curr].get(i);
+      if (vis[e.dest] == false)
+        dfs(graph, e.dest, vis);
 
     }
-     
 
+  }
 
+  public static void main(String args[]) {
+    // no of vertix
+    int V = 7;
 
+    ArrayList<Edge> graph[] = new ArrayList[V];
 
-
-
-
-    public static void main (String args[]) {
-      // no of vertix
-      int V = 7;
-      /* 
+     /* 
            1---- 3
          /       | \
         0       |   5 --6
@@ -137,26 +124,21 @@ public static void addEdge(ArrayList<Edge> graph[],int src,int dest) {
           2 ---4
            */  
 
+    CreatGraph(graph);
 
-      ArrayList<Edge> graph[] = new ArrayList[V];
-      
+    // print 2's neighbour
+    // for(int i =0 ;i < graph[2].size(); i++) {
+    // Edge e = graph[2].get(i);
+    // System.out.println("dest-> "+e.dest+" src->" +e.src);
 
-      CreatGraph(graph);
+    // }
 
+    bfs(graph);
 
-       // print 2's neighbour
-      // for(int i =0 ;i < graph[2].size(); i++) {
-      //   Edge e = graph[2].get(i);
-      //   System.out.println("dest-> "+e.dest+"  src->" +e.src);
+    System.out.println();
+    boolean vis[] = new boolean[V];
 
-      // }
+    dfs(graph, 0, vis);
 
-      bfs(graph);
-
-     System.out.println();
-      boolean vis[] = new boolean[V];
-
-      dfs(graph,0,vis);
-
-    }
   }
+}
